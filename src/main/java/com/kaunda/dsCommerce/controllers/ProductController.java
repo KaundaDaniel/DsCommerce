711c146ff96c543f9974c52fe66d0c2157035f4e
KaundaDaniel <dannydagamaisutic@gmail.com>
2024-01-12 22:43:40 +0100

    Delete Product

--- src/main/java/com/kaunda/dsCommerce/controllers/ProductController.java
+++ src/main/java/com/kaunda/dsCommerce/controllers/ProductController.java
@@ -4,11 +4,11 @@ import com.kaunda.dsCommerce.dto.ProductDTO;
 import com.kaunda.dsCommerce.services.ProductService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,11 +21,34 @@ public class ProductController {
     }
 
     @GetMapping(value = "/{id}")
-    public ProductDTO findById(@PathVariable Long id ){
-        return productService.findById(id);
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO productDTO = productService.findById(id);
+        return ResponseEntity.ok(productDTO);
     }
+
     @GetMapping
-    public Page<ProductDTO> findAll(Pageable pageable){
-        return productService.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        Page<ProductDTO> productDTO = productService.findAll(pageable);
+        return ResponseEntity.ok(productDTO);
+
+    }
+
+    @PostMapping()
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+        dto = productService.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> updade(@PathVariable Long id, @RequestBody ProductDTO dto ) {
+       dto = productService.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id ) {
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/kaunda/dsCommerce/services/ProductService.java
+++ src/main/java/com/kaunda/dsCommerce/services/ProductService.java
@@ -20,12 +20,41 @@ public class ProductService {
     public ProductDTO findById(Long id){
         var product=productRepository.findById(id).get();
         return  new ProductDTO(product);
-
-
     }
     @Transactional(readOnly = true)
     public Page<ProductDTO> findAll(Pageable pageable){
         Page<Product> productList=productRepository.findAll(pageable);
         return productList.map(ProductDTO::new);
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto){
+        var product=new Product();
+        copyDToToEntity(dto, product);
+        product = productRepository.save(product);
+        return  new ProductDTO(product);
+
+    }
+
+    @Transactional
+    public ProductDTO update( Long id, ProductDTO dto){
+        var product= productRepository.getReferenceById(id);
+       copyDToToEntity(dto, product);
+        product = productRepository.save(product);
+        return  new ProductDTO(product);
+
+    }
+    private void copyDToToEntity(ProductDTO productDTO, Product product){
+        product.setName(productDTO.getName());
+        product.setDescription(productDTO.getDescription());
+        product.setPrice(productDTO.getPrice());
+        product.setImgUrl(productDTO.getImgUrl());
+
+    }
+
+    @Transactional
+    public void delete(Long id){
+       productRepository.deleteById(id);
+
     }
 }
